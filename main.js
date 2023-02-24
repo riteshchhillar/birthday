6bb1bc0d764cebec14c8a77028cdb271bf15e3de
Kakaroot <124260803+riteshchhillar@users.noreply.github.com>
2023-02-24 15:52:22 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,16 +3,18 @@ var x= document.getElementById("myAudio");
 
 function new_image()
 {
-
-
-
-
-
-	
-
-
+fabric.Image.fromURL('BirthdayImage.jpg' , function(Img) {
+lbock_image_object=Img;
+block_image_object.scaleTowidth(700);
+block_image_object.scaleTowidth(510);
+block_image_object.set({
+top: 0,
+left: 0
+});
+canvas.add(block_image_object);
+});
 }
 
 function playSound(){
-
+x.play();
 }
